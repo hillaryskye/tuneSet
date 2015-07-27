7df28ac42e104cec4f2f107617dbbc605c89e32b
Hillary Skye <skyeinc@qwestoffice.net>
2015-07-27 01:57:42 -0600

    updated 2nd unirest to include aliases

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,6 @@ var tunesDb = db.get('tunes');
 var qs = require('qs');
 var mime = require('mime');
 
-
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if(req.isAuthenticated()) {
@@ -22,7 +19,13 @@ router.get('/', function(req, res, next) {
   } else {
     res.render('index', {  });
   }
+  tunesDb.find({}, function (err, docs) {
+    console.log(req.params)
+    // not sure I need two res.renders
+    res.render('index', { tunesDB: docs})
+  });
 });
+
 
 router.post('/', function(req, res, next) {
   console.log('req.body from root', req.body)
@@ -58,79 +61,54 @@ router.post('/', function(req, res, next) {
         .end(function (response) {
           key = response.body.settings[0].key
           console.log('res.locals', res.locals)
-          console.log('key', response.body.settings[0])
+          console.log('key', response.body)
           console.log('happy')
+          console.log('req.params.name', req.params)
           tunesDb.insert( response.body.settings[0], function (err, doc) {
               if (err) throw err
               console.log('doc from update', doc)
               // res.render('tunes', doc)
             })
-          res.render('tunes', {tunesSessKey: response.body.settings[0], key: key})
+          res.render('tunes', {tunesSessKey: response.body})
         })
       })
-      // store the document in a variable
-      // doc = tunesDb.findOne({_id})
-      // console.log('doc', doc)
-      // // set a new _id on the document
-      // doc._id = ObjectId("4c8a331bda76c559ef000004")
-      //
-      // // insert the document, using the new _id
-      // db.tunesDb.insert(doc)
-      //
-      // // remove the document with the old _id
-      // db.tunesDb.remove({_id: ObjectId("4cc45467c55f4d2d2a000002")})
-
-
-    // var key =
-
-    // router.get('/:id', function(req, res, next) {
-    //   tunesDb.findOne({ _id: req.params.id }, function (err, doc) {
-    //     if (err) throw err
-    //     console.log('req.session', req.body)
-    //     res.render('tunes', doc)
-    // })
-    //   })
-    //
-    //   router.get('/:id/edit', function(req, res, next) {
-    //     tunesDb.findOne({ id: tuneOnly }, function (err, doc) {
-    //     if (err) throw err
-    //     res.render('tunes', doc)
-    //   })
-    // })
-
-      // router.get('/:id/update', function(req, res, next) {
-          //   db.myusers.update(
-          //   tunesDb.update({ id: tuneOnly }, function (err, doc) {
-          //     if (err) throw err
-          //     console.log('doc from update', doc)
-          //     // res.redirect('')
-          //   })
-          // })
-
-
     })
 })
 
+router.get('/:id', function(req, res, next) {
+  tunesDb.findOne({ _id: req.params.id }, function (err, doc) {
+    if (err) throw err
+    console.log('req.session', req.body)
+    res.render('tunes', doc)
+    })
+  })
 
+router.get(':/id/add', function(req, res, next) {
+  console.log('res', res)
+  res.render('tunes', {})
+})
+// This route is where user adds tunes by keys
+  router.get('/:id/edit', function(req, res, next) {
+    tunesDb.findOne({ _id: req.params.id }, function (err, doc) {
+    if (err) throw err
+    res.render('tunes/add', doc)
+  })
+})
 
-// router.get('/tunes', function(req, res, next) {
-//   console.log('req.body.name from /tunes', tuneName)
-//   // var TUNE_NAME, req.body to uri
-// })
-//
-// router.post('/tunes', function(req, res, next) {
-//   res.redirect('/')
-// })
+  router.post('/:id/delete', function(req, res, next) {
+    tunesDB.remove({ _id: req.params.id }, function (err, docs) {
+      if (err) throw err
+      res.redirect('tunes/add')
+    })
+  })
 
-// router.post('/tunes', function(req, res, next) {
-//   // var TUNE_NAME, req.body to uri
-//   var tune = unirest.get('https://thesession.org/tunes/search?q=drowsy%20maggie')
-//     .end(function (response) {
-//       console.log('req.body', req.body)
-//       console.log('tune', tune)
-//       res.render('tunes', {profile: response.body})
-//     })
-// })
+  router.post('/:id/update', function(req, res, next) {
+    tunesDb.update({ _id: req.params.id }, function (err, doc) {
+      if (err) throw err
+      console.log('doc from update', doc)
+      res.redirect('tunes/add')
+    })
+  })
 
 router.get('/logout', function(req, res, next) {
   req.logout();
